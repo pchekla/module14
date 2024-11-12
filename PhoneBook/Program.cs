18a96e42bfd20373f4dcda7a01f1ad000ec9dcf2
v.sedlachek <v.sedlachek@dodobrands.io>
2024-11-12 07:08:25 +0300

    fix collection

--- PhoneBook/Program.cs
+++ PhoneBook/Program.cs
@@ -6,15 +6,15 @@ class Program
     static void Main(string[] args)
     {
         // создаём пустой список с типом данных Contact
-        var phoneBook = new List<Contact>();
-    
-        // добавляем контакты
-        phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
-        phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
-        phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
-        phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
-        phoneBook.Add(new Contact("Сергей", "Брин", 79990000013, "serg@example.com"));
-        phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 79990000014, "innokentii@example.com"));
+        var phoneBook = new List<Contact>
+        {
+            new("Игорь", "Николаев", 79990000001, "igor@example.com"),
+            new("Сергей", "Довлатов", 79990000010, "serge@example.com"),
+            new("Анатолий", "Карпов", 79990000011, "anatoly@example.com"),
+            new("Валерий", "Леонтьев", 79990000012, "valera@example.com"),
+            new("Сергей", "Брин", 79990000013, "serg@example.com"),
+            new("Иннокентий", "Смоктуновский", 79990000014, "innokentii@example.com")
+        };
 
         // сортировка контактов по имени, а затем по фамилии
         var sortedPhoneBook = phoneBook
